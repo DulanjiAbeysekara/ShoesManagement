249bf91d7468475c60236e2d938ffb665b0d34ac
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-12 22:28:23 +0530

    complete inventoryService  class

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/service/InventoryService.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/service/InventoryService.java
@@ -1,4 +1,19 @@
 package lk.ijse.gdse66.shoeManagement.app.service;
 
+import lk.ijse.gdse66.shoeManagement.app.dto.CustomerDTO;
+import lk.ijse.gdse66.shoeManagement.app.dto.InventoryDTO;
+
+import java.util.List;
+
 public interface InventoryService {
+    List<InventoryDTO> getAllInventory();
+
+    InventoryDTO getInventoryDetails(String id);
+
+    InventoryDTO saveInventory(InventoryDTO inventoryDTO);
+
+    InventoryDTO updateInventory(InventoryDTO inventoryDTO);
+
+    boolean deleteInventory(String id);
+
 }
