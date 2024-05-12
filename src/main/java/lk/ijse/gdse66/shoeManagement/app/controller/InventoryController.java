94f60025d5b654ba55b550115e33dc33ab4a9abc
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-13 00:27:03 +0530

    complete inventoryController  class

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/controller/InventoryController.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/controller/InventoryController.java
@@ -1,4 +1,46 @@
 package lk.ijse.gdse66.shoeManagement.app.controller;
 
+import lk.ijse.gdse66.shoeManagement.app.dto.InventoryDTO;
+import lk.ijse.gdse66.shoeManagement.app.repository.InventoryRepo;
+import lk.ijse.gdse66.shoeManagement.app.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/inventory")
+@CrossOrigin(origins = "*")
 public class InventoryController {
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    public InventoryController() {
+        System.out.println("Inventory Working");
+    }
+
+    @GetMapping("/getAllInventory")
+    List<InventoryDTO> getAllInventory() {
+        System.out.println("request received");
+        return inventoryService.getAllInventory();
+    }
+
+    @PostMapping("/save")
+    public InventoryDTO saveInventory(@RequestBody InventoryDTO inventoryDTO) {
+        System.out.println(inventoryDTO);
+        return inventoryService.saveInventory(inventoryDTO);
+    }
+
+
+    @PatchMapping("/update")
+    public InventoryDTO updateInventory(@RequestBody InventoryDTO inventoryDTO) {
+        System.out.println(inventoryDTO);
+        return inventoryService.updateInventory(inventoryDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteInventory(@PathVariable(value = "id") String id){
+        inventoryService.deleteInventory(id);
+    }
 }
--- src/main/java/lk/ijse/gdse66/shoeManagement/app/service/impl/InventoryServiceImpl.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/service/impl/InventoryServiceImpl.java
@@ -1,4 +1,70 @@
 package lk.ijse.gdse66.shoeManagement.app.service.impl;
 
-public class InventoryServiceImpl {
+import lk.ijse.gdse66.shoeManagement.app.dto.CustomerDTO;
+import lk.ijse.gdse66.shoeManagement.app.dto.InventoryDTO;
+import lk.ijse.gdse66.shoeManagement.app.entity.InventoryEntity;
+import lk.ijse.gdse66.shoeManagement.app.repository.InventoryRepo;
+import lk.ijse.gdse66.shoeManagement.app.service.InventoryService;
+import lk.ijse.gdse66.shoeManagement.app.service.exception.DuplicateRecordException;
+import lk.ijse.gdse66.shoeManagement.app.service.exception.NotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InventoryServiceImpl implements InventoryService {
+
+    @Autowired
+    private InventoryRepo inventoryRepo;
+    @Autowired
+    private ModelMapper mapper;
+
+    public InventoryServiceImpl(InventoryRepo inventoryRepo, ModelMapper mapper) {
+        this.inventoryRepo = inventoryRepo;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public List<InventoryDTO> getAllInventory() {
+        return inventoryRepo.findAll().stream().map(
+                inventory -> mapper.map(inventory, InventoryDTO.class)).toList();
+    }
+
+    @Override
+    public InventoryDTO getInventoryDetails(String id) {
+        return null;
+    }
+
+    @Override
+    public InventoryDTO saveInventory(InventoryDTO inventoryDTO) {
+        if (inventoryRepo.existsById(inventoryDTO.getItemCode())){
+            throw new DuplicateRecordException("Inventory ID is Already Exist");
+        }
+
+        System.out.println(inventoryDTO);
+        return mapper.map(inventoryRepo.save(mapper.map(inventoryDTO, InventoryEntity.class)),InventoryDTO.class);
+    }
+
+    @Override
+    public InventoryDTO updateInventory(InventoryDTO inventoryDTO) {
+        if (!inventoryRepo.existsById(inventoryDTO.getItemCode())){
+            throw new NotFoundException("Can't find inventory id !!");
+        }
+
+        InventoryEntity inventoryEntity=inventoryRepo.findById(inventoryDTO.getItemCode()).get();
+        System.out.println("inventory is "+inventoryEntity);
+
+        return mapper.map(inventoryRepo.save(mapper.map(inventoryDTO,InventoryEntity.class)),InventoryDTO.class);
+    }
+
+    @Override
+    public boolean deleteInventory(String id) {
+        if (!inventoryRepo.existsById(id)){
+            throw new NotFoundException("Can't find inventory id!!!");
+        }
+        inventoryRepo.deleteById(id);
+        return true;
+    }
 }
