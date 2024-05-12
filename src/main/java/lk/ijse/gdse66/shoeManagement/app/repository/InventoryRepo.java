17f4a16c904a53981f8b978946add88a444694b8
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-12 22:22:19 +0530

    complete inventoryRepo clas

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/InventoryRepo.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/InventoryRepo.java
@@ -1,4 +1,7 @@
 package lk.ijse.gdse66.shoeManagement.app.repository;
 
-public interface InventoryRepo {
+import lk.ijse.gdse66.shoeManagement.app.entity.InventoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepo extends JpaRepository<InventoryEntity,String> {
 }
