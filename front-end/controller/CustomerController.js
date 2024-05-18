f84d36bf2895d22b53f95ecccd366c1d56692987
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-18 10:40:50 +0530

    finished update front end in customer

--- front-end/controller/CustomerController.js
+++ front-end/controller/CustomerController.js
@@ -37,9 +37,53 @@ $('#btnSave').click(function () {
     });
 });
 
+$('#btnUpdate').click(function (){
+    let customerCode = $('#txtCustomerCode').val();
+    let customerName = $('#txtCustomerName').val();
+    let gender = $('#txtGender').find(':selected').val();
+    // let joinDateLoyaltyCustomer = $('#txtJoinDateLoyaltyCustomer').val();
+    let level = $('#txtLevel').find(':selected').val();
+    let totalPoints = $('#txtTotalPoints').val();
+    // let dateOfBirth = $('#txtDateOfBirth').val();
+    let address = $('#txtAddress').val();
+
+
+    var customer = {
+        customerCode: customerCode,
+        customerName: customerName,
+        gender: gender,
+        // joinDateLoyaltyCustomer: joinDateLoyaltyCustomer,
+        level: level,
+        totalPoints: totalPoints,
+        // dateOfBirth: dateOfBirth,
+        address: address
+    };
+
+    $('btnDelete').click(function (){
+
+    });
+
+    $.ajax({
+        url: 'http://localhost:8080/app1/cust/update',
+        type: 'PATCH',
+        contentType: 'application/json',
+        data: JSON.stringify(customer),
+        success: function (response) {
+            alert('Customer information updated successfully!');
+            console.log(customer);
+        },
+        error: function (xhr, status, error) {
+            console.error('Error updating customer information:', error);
+            alert('Customer Not Found!');
+        }
+    });
+});
+
 
 $('#btnGetAll').click(function (){
     for (var customer of customerArray) {
+
+        $('#customerTable').empty();
 
         console.log(customer.customer_code);
 
