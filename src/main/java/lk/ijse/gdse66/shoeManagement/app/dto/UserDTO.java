f872df7358bf26e92a0901a446d2e947742f3d12
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-19 20:44:13 +0530

    creatinguserrepo

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/dto/UserDTO.java
@@ -1,4 +1,15 @@
 package lk.ijse.gdse66.shoeManagement.app.dto;
 
-public class UserDTO {
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lk.ijse.gdse66.shoeManagement.app.util.AccessRole;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private String userId;
+    private String email;
+    private String password;
+    private AccessRole accessRole;
+
 }
--- src/main/java/lk/ijse/gdse66/shoeManagement/app/entity/UserEntity.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/entity/UserEntity.java
@@ -1,4 +1,15 @@
 package lk.ijse.gdse66.shoeManagement.app.entity;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lk.ijse.gdse66.shoeManagement.app.util.AccessRole;
+
 public class UserEntity {
+    @Id
+    private String userId;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private AccessRole accessRole;
 }
