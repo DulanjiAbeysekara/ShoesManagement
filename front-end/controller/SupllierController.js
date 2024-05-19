95678594f4b94c9c8be23bb20b65b5a30794d05b
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-19 12:42:01 +0530

    finished customer saved

--- front-end/controller/SupllierController.js
+++ front-end/controller/SupllierController.js
@@ -6,4 +6,31 @@ $('#btnSupSave').click(function (){
     let contactNo1=$('#txtContactNo1').val();
     let contactNo2=$('#txtContactNo2').val();
     let email=$('#txtEmail').val();
+
+    var supplier={
+        code:code,
+        name:name,
+        category:category,
+        address:address,
+        contactNo1:contactNo1,
+        contactNo2:contactNo2,
+        email:email,
+    }
+
+
+    $.ajax({
+        url: 'http://localhost:8080/app1/supplier/save',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(supplier),
+        success: function (response) {
+            alert('Supplier information saved successfully!');
+            console.log(supplier);
+            getAll();
+        },
+        error: function (xhr, status, error) {
+            console.error('Error saving supplier information:', error);
+            alert('Supplier Not Found!');
+        }
+    });
 });
