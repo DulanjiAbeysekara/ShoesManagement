6ded70c48bc9109dbf08219a3363445fff2ad9e8
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-19 12:35:40 +0530

    sets the text feild in supplier

---
+++ front-end/controller/SupllierController.js
@@ -0,0 +1,9 @@
+$('#btnSupSave').click(function (){
+    let code=$('#txtSupplierCode').val();
+    let name=$('#txtSupplierName').val();
+    let category=$('#txtCategory').val();
+    let  address=$('#txtSupplierAddress').val();
+    let contactNo1=$('#txtContactNo1').val();
+    let contactNo2=$('#txtContactNo2').val();
+    let email=$('#txtEmail').val();
+});
