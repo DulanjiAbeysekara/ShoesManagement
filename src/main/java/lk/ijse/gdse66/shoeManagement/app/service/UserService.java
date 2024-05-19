0410c2ccb6cd9f115a8070b2b3e6708cf0330d8f
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-19 21:15:35 +0530

    creating userService class

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/service/UserService.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/service/UserService.java
@@ -1,4 +1,20 @@
 package lk.ijse.gdse66.shoeManagement.app.service;
 
+import lk.ijse.gdse66.shoeManagement.app.dto.CustomerDTO;
+import lk.ijse.gdse66.shoeManagement.app.dto.UserDTO;
+
+import java.util.List;
+
 public interface UserService {
+    List<UserDTO> getAllUsers();
+
+    UserDTO getUserDetails(String id);
+
+    UserDTO saveUser(UserDTO userDTO);
+
+    UserDTO updateUser(UserDTO userDTO);
+
+    boolean deleteUser(String id);
+
+//    List<UserDTO> searchUser(String customerName);
 }
