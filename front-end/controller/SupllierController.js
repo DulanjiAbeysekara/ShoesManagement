aacb72f72c458e3cb09e45961e0dfa0282348dd7
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-19 13:02:49 +0530

    creating update

--- front-end/controller/SupllierController.js
+++ front-end/controller/SupllierController.js
@@ -34,3 +34,44 @@ $('#btnSupSave').click(function (){
         }
     });
 });
+
+
+$('#btnSupUpdate').click(function (){
+    let code=$('#txtSupplierCode').val();
+    let name=$('#txtSupplierName').val();
+    let category=$('#txtCategory').val();
+    let  address=$('#txtSupplierAddress').val();
+    let contactNo1=$('#txtContactNo1').val();
+    let contactNo2=$('#txtContactNo2').val();
+    let email=$('#txtEmail').val();
+
+    var supplier={
+        code:code,
+        name:name,
+        category:category,
+        address:address,
+        contactNo1:contactNo1,
+        contactNo2:contactNo2,
+        email:email,
+    }
+
+    $.ajax({
+        url: 'http://localhost:8080/app1/supplier/update',
+        type: 'PATCH',
+        contentType: 'application/json',
+        data: JSON.stringify(supplier),
+        success: function (response) {
+            alert('Supplier information updated successfully!');
+            console.log(supplier);
+            getAll();
+        },
+        error: function (xhr, status, error) {
+            console.error('Error updating supplier information:', error);
+            alert('Supplier Not Found!');
+        }
+    });
+
+});
+
+
+$('#btnSupDlt')
