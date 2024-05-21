c7dcd24226d3be6ef3c5b4729bae7c59da2fcc31
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-22 01:11:14 +0530

    creating getll employee class

--- front-end/controller/EmployeeController.js
+++ front-end/controller/EmployeeController.js
@@ -1,12 +1,9 @@
 
 $(document).ready(function () {
-
     getAll();
 
     $('#employeeSaveBtn').click(function () {
-
-        let employee =  getEmployeeDataFromForm();
-
+        let employee = getEmployeeDataFromForm();
 
         $.ajax({
             url: 'http://localhost:8080/app1/employee/save',
@@ -18,19 +15,16 @@ $(document).ready(function () {
                 console.log(employee);
                 clearFields();
                 getAll();
-
             },
             error: function (xhr, status, error) {
                 console.error('Error saving employee information:', error);
                 alert('Employee Not Found!');
             }
         });
-
     });
 
-
     $('#btnEmUpdate').click(function () {
-        let employee =  getEmployeeDataFromForm();
+        let employee = getEmployeeDataFromForm();
 
         $.ajax({
             url: 'http://localhost:8080/app1/employee/update',
@@ -38,20 +32,17 @@ $(document).ready(function () {
             contentType: 'application/json',
             data: JSON.stringify(employee),
             success: function (response) {
-                alert('Customer information updated successfully!');
+                alert('Employee information updated successfully!');
                 console.log(employee);
                 clearFields();
                 getAll();
-
             },
             error: function (xhr, status, error) {
                 console.error('Error updating employee information:', error);
                 alert('Employee Not Found!');
             }
         });
-
     });
-
 
     $('#btnEmDelete').click(function () {
         let employeeCode = $('#txtEmployeeCode').val();
@@ -72,25 +63,23 @@ $(document).ready(function () {
         });
     });
 
+    // $('#txtEmployeePicture').change(function() {
+    //     var fileInput = $('#txtEmployeePicture')[0];
+    //     var file = fileInput.files[0];
 
-//
-// $('#txtEmployeePicture').change(function() {
-//     var fileInput = $('#txtEmployeePicture')[0];
-//     var file = fileInput.files[0];
-//
-//     if (file && (file.type.includes('image') || file.type === 'image/gif')) {
-//         var reader = new FileReader();
-//         reader.onload = function (e) {
-//             $('#img').attr('src', e.target.result);
-//         };
-//         reader.readAsDataURL(file);
-//         $(this).val("");
-//     } else {
-//         // Handle error scenario
-//     }
-// });
+    //     if (file && (file.type.includes('image') || file.type === 'image/gif')) {
+    //         var reader = new FileReader();
+    //         reader.onload = function (e) {
+    //             $('#img').attr('src', e.target.result);
+    //         };
+    //         reader.readAsDataURL(file);
+    //         $(this).val("");
+    //     } else {
+    //         // Handle error scenario
+    //     }
+    // });
 
-////////////////////////////////methods////////////////////////////////////
+    //////////////////////////////////methods////////////////////////////////////
     function getAll() {
         $('#tblEmployee tbody').empty();
 
@@ -111,7 +100,6 @@ $(document).ready(function () {
                                 <td>${employee.attachedBranch}</td>
                                 <td>${employee.address}</td>
                                 <td>${employee.contactNo}</td>
-                           
                             </tr>`;
                     $('#tblEmployee tbody').append(row);
                 }
@@ -130,9 +118,9 @@ $(document).ready(function () {
             let gender = $(this).children(':nth-child(3)').text();
             let status = $(this).children(':nth-child(4)').text();
             let designation = $(this).children(':nth-child(5)').text();
-            let dob = $(this).children(':nth-child(6)').text();
-            let dateOfJoin = $(this).children(':nth-child(7)').text();
-            let accessRole = $(this).children(':nth-child(8)').text();
+            let accessRole = $(this).children(':nth-child(6)').text();
+            let dob = $(this).children(':nth-child(7)').text();
+            let dateOfJoin = $(this).children(':nth-child(8)').text();
             let attachedBranch = $(this).children(':nth-child(9)').text();
             let address = $(this).children(':nth-child(10)').text();
             let contactNo = $(this).children(':nth-child(11)').text();
@@ -142,9 +130,9 @@ $(document).ready(function () {
             $('#txtEmployeeGender').val(gender);
             $('#txtEmployeeStatus').val(status);
             $('#txtEmployeeDesignation').val(designation);
+            $('#txtAccessRole').val(accessRole);
             $('#txtEmployeeDateOfBirth').val(dob);
             $('#txtEmployeeDateOfJoin').val(dateOfJoin);
-            $('#txtAccessRole').val(accessRole);
             $('#txtAttachedBranch').val(attachedBranch);
             $('#txtAddress').val(address);
             $('#txtContactNo').val(contactNo);
@@ -152,38 +140,34 @@ $(document).ready(function () {
     }
 
     function clearFields() {
-            $('#txtEmployeeCode').val(),
-            $('#txtEmployeeName').val(),
-            $('#txtEmployeeGender').val(),
-            $('#txtEmployeeStatus').val(),
-            $('#txtEmployeeDesignation').val(),
-            $('#txtAccessRole').val(),
-            $('#txtEmployeeDateOfBirth').val(),
-            $('#txtEmployeeDateOfJoin').val(),
-            $('#txtAttachedBranch').val(),
-            $('#txtAddress').val(),
-            $('#txtContactNo').val()
+        $('#txtEmployeeCode').val('');
+        $('#txtEmployeeName').val('');
+        $('#txtEmployeeGender').val('');
+        $('#txtEmployeeStatus').val('');
+        $('#txtEmployeeDesignation').val('');
+        $('#txtAccessRole').val('');
+        $('#txtEmployeeDateOfBirth').val('');
+        $('#txtEmployeeDateOfJoin').val('');
+        $('#txtAttachedBranch').val('');
+        $('#txtAddress').val('');
+        $('#txtContactNo').val('');
 
         $("#txtEmployeeCode").focus();
     }
 
     function getEmployeeDataFromForm() {
         return {
-
-            employeeCode : $('#txtEmployeeCode').val(),
-            employeeName : $('#txtEmployeeName').val(),
-            gender : $('#txtEmployeeGender').find(':selected').val(),
-            status : $('#txtEmployeeStatus').val(),
-            designation : $('#txtEmployeeDesignation').val(),
-            accessRole :$('#txtAccessRole').find(':selected').val(),
-            dob : $('#txtEmployeeDateOfBirth').val(),
-            dateOfJoin : $('#txtEmployeeDateOfJoin').val(),
-            attachedBranch : $('#txtAttachedBranch').val(),
-            address : $('#txtAddress').val(),
-            contactNo : $('#txtContactNo').val()
-
+            employeeCode: $('#txtEmployeeCode').val(),
+            employeeName: $('#txtEmployeeName').val(),
+            gender: $('#txtEmployeeGender').find(':selected').val(),
+            status: $('#txtEmployeeStatus').val(),
+            designation: $('#txtEmployeeDesignation').val(),
+            accessRole: $('#txtAccessRole').find(':selected').val(),
+            dob: $('#txtEmployeeDateOfBirth').val(),
+            dateOfJoin: $('#txtEmployeeDateOfJoin').val(),
+            attachedBranch: $('#txtAttachedBranch').val(),
+            address: $('#txtAddress').val(),
+            contactNo: $('#txtContactNo').val()
         };
     }
-
-
 });
