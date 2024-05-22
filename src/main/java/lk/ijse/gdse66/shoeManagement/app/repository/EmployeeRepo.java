854ada19dfdddde99b89746e770fe1fe1b93d255
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-22 10:40:42 +0530

    creating employee repo generated

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/EmployeeRepo.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/EmployeeRepo.java
@@ -2,6 +2,11 @@ package lk.ijse.gdse66.shoeManagement.app.repository;
 
 import lk.ijse.gdse66.shoeManagement.app.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepo extends JpaRepository<EmployeeEntity,String> {
+    @Query(value = "SELECT employee_code FROM employee ORDER BY employee_code DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
+
+
 }
