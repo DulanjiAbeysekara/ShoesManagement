31e4b930d803a4871a196d4fbc1147746148d0c7
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-22 10:33:09 +0530

    creating getll supplier method class

--- front-end/controller/SupllierController.js
+++ front-end/controller/SupllierController.js
@@ -1,113 +1,119 @@
-getAll();
 
-$('#btnSupSave').click(function (){
-    let code=$('#txtSupplierCode').val();
-    let name=$('#txtSupplierName').val();
-    let category=$('#txtCategory').val();
-    let  address=$('#txtSupplierAddress').val();
-    let contactNo1=$('#txtContactNo1').val();
-    let contactNo2=$('#txtContactNo2').val();
-    let email=$('#txtEmail').val();
+$(document).ready(function () {
 
-    var supplier={
-        code:code,
-        name:name,
-        category:category,
-        address:address,
-        contactNo1:contactNo1,
-        contactNo2:contactNo2,
-        email:email,
-    }
+    getAll();
 
+    $('#btnSupSave').click(function () {
+        let code = $('#txtSupplierCode').val();
+        let name = $('#txtSupplierName').val();
+        let category = $('#txtCategory').val();
+        let address = $('#txtSupplierAddress').val();
+        let contactNo1 = $('#txtContactNo1').val();
+        let contactNo2 = $('#txtContactNo2').val();
+        let email = $('#txtEmail').val();
 
-    $.ajax({
-        url: 'http://localhost:8080/app1/supplier/save',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(supplier),
-        success: function (response) {
-            alert('Supplier information saved successfully!');
-            console.log(supplier);
-            getAll();
-        },
-        error: function (xhr, status, error) {
-            console.error('Error saving supplier information:', error);
-            alert('Supplier Not Found!');
+        var supplier = {
+            code: code,
+            name: name,
+            category: category,
+            address: address,
+            contactNo1: contactNo1,
+            contactNo2: contactNo2,
+            email: email,
         }
-    });
-});
 
 
-$('#btnSupUpdate').click(function (){
-    let code=$('#txtSupplierCode').val();
-    let name=$('#txtSupplierName').val();
-    let category=$('#txtCategory').val();
-    let  address=$('#txtSupplierAddress').val();
-    let contactNo1=$('#txtContactNo1').val();
-    let contactNo2=$('#txtContactNo2').val();
-    let email=$('#txtEmail').val();
-
-    var supplier={
-        code:code,
-        name:name,
-        category:category,
-        address:address,
-        contactNo1:contactNo1,
-        contactNo2:contactNo2,
-        email:email,
-    }
-
-    $.ajax({
-        url: 'http://localhost:8080/app1/supplier/update',
-        type: 'PATCH',
-        contentType: 'application/json',
-        data: JSON.stringify(supplier),
-        success: function (response) {
-            alert('Supplier information updated successfully!');
-            console.log(supplier);
-            getAll();
-        },
-        error: function (xhr, status, error) {
-            console.error('Error updating supplier information:', error);
-            alert('Supplier Not Found!');
-        }
+        $.ajax({
+            url: 'http://localhost:8080/app1/supplier/save',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(supplier),
+            success: function (response) {
+                alert('Supplier information saved successfully!');
+                console.log(supplier);
+                clearFields();
+                getAll();
+            },
+            error: function (xhr, status, error) {
+                console.error('Error saving supplier information:', error);
+                alert('Supplier Not Found!');
+            }
+        });
     });
 
-});
 
+    $('#btnSupUpdate').click(function () {
+        let code = $('#txtSupplierCode').val();
+        let name = $('#txtSupplierName').val();
+        let category = $('#txtCategory').val();
+        let address = $('#txtSupplierAddress').val();
+        let contactNo1 = $('#txtContactNo1').val();
+        let contactNo2 = $('#txtContactNo2').val();
+        let email = $('#txtEmail').val();
 
-$('#btnSupDlt').click(function (){
-
-    let code=$('#txtSupplierCode').val();
-
-    $.ajax({
-        url: 'http://localhost:8080/app1/supplier/' + code,
-        type: 'DELETE',
-        success: function (response) {
-            alert('Supplier information deleted successfully!');
-            console.log('Deleted supplier with code:', code);
-            getAll();
-        },
-        error: function (xhr, status, error) {
-            console.error('Error deleting Supplier information:', error);
-            alert('Customer Not Found!');
+        var supplier = {
+            code: code,
+            name: name,
+            category: category,
+            address: address,
+            contactNo1: contactNo1,
+            contactNo2: contactNo2,
+            email: email,
         }
+
+        $.ajax({
+            url: 'http://localhost:8080/app1/supplier/update',
+            type: 'PATCH',
+            contentType: 'application/json',
+            data: JSON.stringify(supplier),
+            success: function (response) {
+                alert('Supplier information updated successfully!');
+                console.log(supplier);
+                clearFields();
+                getAll();
+            },
+            error: function (xhr, status, error) {
+                console.error('Error updating supplier information:', error);
+                alert('Supplier Not Found!');
+            }
+        });
+
     });
 
-});
+
+    $('#btnSupDlt').click(function () {
+
+        let code = $('#txtSupplierCode').val();
+
+        $.ajax({
+            url: 'http://localhost:8080/app1/supplier/' + code,
+            type: 'DELETE',
+            success: function (response) {
+                alert('Supplier information deleted successfully!');
+                console.log('Deleted supplier with code:', code);
+                clearFields();
+                getAll();
+            },
+            error: function (xhr, status, error) {
+                console.error('Error deleting Supplier information:', error);
+                alert('Customer Not Found!');
+            }
+        });
+
+    });
 
 
-//////////////////////////getAll//////////////////////////////////////////
+//////////////////////////methods//////////////////////////////////////////
 
-function getAll() {
-    $('#tblSupplier tbody').empty();
+    function getAll() {
+        $('#tblSupplier tbody').empty();
 
-    $.ajax({
-        url: "http://localhost:8080/app1/supplier/getAllSupplier",
-        method: "GET",
-        success: function (resp) {
-            for (const supplier of resp) {
-                let row = `<tr>
+        $.ajax({
+            url: "http://localhost:8080/app1/supplier/getAllSupplier",
+            method: "GET",
+            success: function (resp) {
+                for (const supplier of resp) {
+                    let row = `<tr>
                                 <td>${supplier.code}</td>
                                 <td>${supplier.name}</td>
                                 <td>${supplier.category}</td>
@@ -117,34 +123,49 @@ function getAll() {
                                 <td>${supplier.email}</td>
                                 
                             </tr>`;
-                $('#tblSupplier tbody').append(row);
+                    $('#tblSupplier tbody').append(row);
+                }
+                bindClickEvents();
+            },
+            error: function (error) {
+                console.log("Error: ", error);
             }
-            bindClickEvents();
-        },
-        error: function (error) {
-            console.log("Error: ", error);
-        }
-    });
-}
+        });
+    }
 
-function bindClickEvents() {
-    $('#tblSupplier tbody>tr').click(function () {
-        let code = $(this).children(':nth-child(1)').text();
-        let name = $(this).children(':nth-child(2)').text();
-        let category = $(this).children(':nth-child(3)').text();
-        let address = $(this).children(':nth-child(4)').text();
-        let contactNo1 = $(this).children(':nth-child(5)').text();
-        let contactNo2 = $(this).children(':nth-child(6)').text();
-        let email = $(this).children(':nth-child(7)').text();
+    function bindClickEvents() {
+        $('#tblSupplier tbody>tr').click(function () {
+            let code = $(this).children(':nth-child(1)').text();
+            let name = $(this).children(':nth-child(2)').text();
+            let category = $(this).children(':nth-child(3)').text();
+            let address = $(this).children(':nth-child(4)').text();
+            let contactNo1 = $(this).children(':nth-child(5)').text();
+            let contactNo2 = $(this).children(':nth-child(6)').text();
+            let email = $(this).children(':nth-child(7)').text();
 
 
-        $('#txtSupplierCode').val(code);
-        $('#txtSupplierName').val(name);
-        $('#txtCategory').val(category);
-        $('#txtSupplierAddress').val(address);
-        $('#txtContactNo1').val(contactNo1);
-        $('#txtContactNo2').val(contactNo2);
-        $('#txtEmail').val(email);
+            $('#txtSupplierCode').val(code);
+            $('#txtSupplierName').val(name);
+            $('#txtCategory').val(category);
+            $('#txtSupplierAddress').val(address);
+            $('#txtContactNo1').val(contactNo1);
+            $('#txtContactNo2').val(contactNo2);
+            $('#txtEmail').val(email);
 
-    });
-}
+        });
+    }
+
+    function clearFields() {
+
+        $('#txtSupplierCode').val("");
+        $('#txtSupplierName').val("");
+        $('#txtCategory').val("");
+        $('#txtSupplierAddress').val("");
+        $('#txtContactNo1').val("");
+        $('#txtContactNo2').val("");
+        $('#txtEmail').val("");
+
+        $("#txtSupplierCode").focus();
+    }
+
+});
