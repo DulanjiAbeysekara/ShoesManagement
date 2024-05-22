9f65d86d7941f084be901836fb448dd9605a2f94
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-22 12:22:27 +0530

    creating supplierRepo generate method

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/SupplierRepo.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/SupplierRepo.java
@@ -3,6 +3,10 @@ package lk.ijse.gdse66.shoeManagement.app.repository;
 import lk.ijse.gdse66.shoeManagement.app.entity.CustomerEntity;
 import lk.ijse.gdse66.shoeManagement.app.entity.SupplierEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SupplierRepo  extends JpaRepository<SupplierEntity,String> {
+
+    @Query(value = "SELECT code FROM supplier ORDER BY code DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
 }
