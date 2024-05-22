0f465d75afa803a044b9bb1cf3f6cfc6d53f03da
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-22 11:36:23 +0530

    inventory repo creating genetrate id

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/InventoryRepo.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/InventoryRepo.java
@@ -2,6 +2,11 @@ package lk.ijse.gdse66.shoeManagement.app.repository;
 
 import lk.ijse.gdse66.shoeManagement.app.entity.InventoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InventoryRepo extends JpaRepository<InventoryEntity,String> {
+
+    @Query(value = "SELECT item_code FROM inventory ORDER BY item_code DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
+
 }
