a92430bc5b51f4ad3b7ee5936e595485a97562c5
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-23 11:48:29 +0530

    create sale

---
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/entity/SalesEntity.java
@@ -0,0 +1,45 @@
+package lk.ijse.gdse66.shoeManagement.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "sales")
+public class SalesEntity{
+
+    @Id
+    @Column(name = "order_no")
+    private String orderNo;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "total_price")
+    private Double totalPrice;
+
+    @Column(name = "purchase_date")
+    private Date purchaseDate;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    @Column(name = "added_points")
+    private Double addedPoints;
+
+    @Column(name = "cashier_name")
+    private String cashierName;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy =  "sales")
+    private List<SalesDetailsEntity> salesDetails = new ArrayList<>();
+}
