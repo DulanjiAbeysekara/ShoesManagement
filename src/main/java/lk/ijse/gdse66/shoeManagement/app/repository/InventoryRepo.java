ba1ac73ddee55a169da9e5a8ccd9e1702f2b1f6b
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-27 21:37:02 +0530

    creating search method in item repo

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/InventoryRepo.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/InventoryRepo.java
@@ -1,12 +1,18 @@
 package lk.ijse.gdse66.shoeManagement.app.repository;
 
+import lk.ijse.gdse66.shoeManagement.app.entity.CustomerEntity;
 import lk.ijse.gdse66.shoeManagement.app.entity.InventoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface InventoryRepo extends JpaRepository<InventoryEntity,String> {
 
     @Query(value = "SELECT item_code FROM inventory ORDER BY item_code DESC LIMIT 1", nativeQuery = true)
     String getLastIndex();
+
+    List<InventoryEntity> findByInventoryIdStaringWith(String id);
+
 
 }
