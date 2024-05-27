9e83d975be1fa149aaf8db2d8e97c72ce9774d07
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-05-27 23:27:07 +0530

    craeting serach method in supplier repo

--- src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/SupplierRepo.java
+++ src/main/java/lk/ijse/gdse66/shoeManagement/app/repository/SupplierRepo.java
@@ -2,12 +2,19 @@ package lk.ijse.gdse66.shoeManagement.app.repository;
 
 import lk.ijse.gdse66.shoeManagement.app.entity.CustomerEntity;
 //import lk.ijse.gdse66.shoeManagement.app.entity.SupplierEntity;
+import lk.ijse.gdse66.shoeManagement.app.entity.EmployeeEntity;
 import lk.ijse.gdse66.shoeManagement.app.entity.SupplierEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SupplierRepo  extends JpaRepository<SupplierEntity,String> {
 
     @Query(value = "SELECT code FROM supplier ORDER BY code DESC LIMIT 1", nativeQuery = true)
     String getLastIndex();
+
+    List<SupplierEntity> findByCodeStartingWith(String id);
+
 }
+
